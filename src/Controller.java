68c107b48ab662e46cd8e969ca849ee0a1ad1980
andrijaitis <andrijaitis92@gmail.com>
2017-05-09 14:18:02 +0200

    shit

---
+++ src/Controller.java
@@ -0,0 +1,11 @@
+import javafx.fxml.FXML;
+
+/**
+ * Created by Vidas on 5/9/2017.
+ */
+
+
+public class Controller {
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Vidas on 5/9/2017.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("nordicGUI.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 900, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
