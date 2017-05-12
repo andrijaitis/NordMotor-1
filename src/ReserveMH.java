f60b5108e5606d1a3707322c7f35be3e5fdacafb
Mantas_MSI <mantasling@gmail.com>
2017-05-12 15:39:19 +0200

    commit6_byMantas_seasons_fixed_fxml

--- src/ReserveMH.java
+++ src/ReserveMH.java
@@ -12,7 +12,7 @@ public class ReserveMH {
         return " Peak season";
     }else if(startMonth >=13 || startMonth <= 0){
         System.out.println(" You entered a non existing month you fcking donut");//such a month does not exist
-        return " ";
+        return "Wrong Date ass";
     }else if( startMonth == 12 || startMonth == 2 || startMonth == 1 ){
         System.out.println("winter bitch");//low season winter
         return " Low season";
